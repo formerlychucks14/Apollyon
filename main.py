0d4d7f421a6bb40aee41a4605982e64c52ca8e6e
formerlychucks14 <86799408+formerlychucks14@users.noreply.github.com>
2021-07-18 17:27:16 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+import amino
+import pyfiglet
+import colorama
+from colorama import Fore, Back, Style
+import threading 
+import sys
+import PIL
+from PIL import Image
+from amino import util, lib
+
+print(Fore.BLUE)
+print(Style.BRIGHT)
+
+Introduction = pyfiglet.figlet_format("Apollyon", font="colossal")
+print(Introduction)
+print("Script made by Sneed. Have fun (:")
+
+email = input("Bot Email: ")
+password = input("Bot Password: ")
+
+client = amino.Client()
+client.login(email=email, password=password)
+print("Logged in the bot account successfully.")
+
+subclients = client.sub_clients()
+for name, id in zip(subclients.name, subclients.comId):
+    print(name, id)
+
+comId = input("Community ID: ")
+sub_client = amino.SubClient(comId=comId, profile=client.profile)
+print("Accessed the Community.")
+
+title = input("Wiki Title: ")
+content = input("Wiki Content: ")
+
+def WikiSpammer():
+    while True:
+     try:
+        sub_client.post_wiki(title=title, content=content, imageList=[open("rittammorgen.jpg", "rb")])
+     finally:
+         pass
+         print("The community is being trolled. Good luck.")
+
+threads = []
+
+for i in range(50):
+    t = threading.Thread(target=WikiSpammer)
+    t.daemon = False
+    threads.append(t)
+
+for i in range(50):
+    threads[i].start()
+
+for i in range(50):
+    threads[i].join()
+
+
+
+
+
+
